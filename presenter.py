f0e6bfaa0ff7dafcc79234fc21c029917c261900
Ainur <ainuralmm@gmail.com>
2025-09-10 15:07:32 +0200

    made some updates

--- presenter.py
+++ presenter.py
@@ -1,29 +1,33 @@
 from __future__ import annotations
+
 from typing import Protocol
+
 from model import Model
 
+
 class View(Protocol):
-    def init_ui (self, presenter:Presenter) -> None:
+    def init_ui(self, presenter: Presenter) -> None:
         ...
 
     def get_entry_text(self) -> str:
         ...
 
-    def  clear_entry(self) -> None:
+    def clear_entry(self) -> None:
         ...
 
-    def update_task_list (self,tasks:list[str]) -> None:
+    def update_task_list(self, tasks: list[str]) -> None:
         ...
 
     @property
-    def selected_tasks(self) -> str:
+    def selected_task(self) -> str:
         ...
 
     def mainloop(self) -> None:
         ...
 
+
 class Presenter:
-    def __init__(self, model:Model, view:View) -> None:
+    def __init__(self, model: Model, view: View) -> None:
         self.model = model
         self.view = view
 
@@ -36,7 +40,6 @@ class Presenter:
     def handle_delete_task(self, event=None) -> None:
         self.model.delete_task(self.view.selected_task)
         self.update_task_list()
-
 
     def update_task_list(self) -> None:
         tasks = self.model.get_tasks()
