e3b4d0e7f7a7e9cf8c5681163a8b3ad53f0d8591
Ainur <ainuralmm@gmail.com>
2025-09-18 12:58:13 +0200

    Streamlit re-runs the script every time the user interacts. To avoid accidentally inserting into Oracle multiple times, st.session_state to store the fact that submission already happened

--- Test MVP (Selenium,Streamlit)/view.py
+++ Test MVP (Selenium,Streamlit)/view.py
@@ -1,22 +1,21 @@
 import streamlit as st
 from datetime import date
 
-
+# Page setup (run once, at the start)
+st.set_page_config(layout='centered')
 st.image("logo-agsm.jpg", width=200)
 
 class CourseView:
     def __init__(self):
-        st.set_page_config(layout='centered')
         st.title("Oracle Course Management Automator")
 
     def render_form(self):
         # This method displays the input form and returns the collected data.
-
         st.header("Enter Course Details")
 
         # We use a Streamlit form to group the inputs.
         # The code inside 'with form:' will only run when the submit button is pressed.
-        with st.form(key='course_form'):
+        with st.form(key='course_creation_form'):
             # These are the input fields for the user.
             course_title = st.text_input("Course Title", "Example: Data Analytics")
             programme = st.text_area("Programme Details", "OPTIONAL FIELD: IF NEEDED, INSERT IMPORTANT INFORMATION about the course")
@@ -35,6 +34,7 @@ class CourseView:
                 "short_description": short_desc,
                 "start_date": start_date
             }
+            st.session_state["course_details"] = course_details
             return course_details
 
         #if the button has not been pressed,return None
