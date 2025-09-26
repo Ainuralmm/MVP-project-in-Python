8203b56be4b2a3d8a8729878f47c5b32e715c0ef
Ainur <ainuralmm@gmail.com>
2025-09-26 10:21:14 +0200

    Changed the date input field which shows italian format DD/MM/YYYY

--- Test MVP (Selenium,Streamlit)/view.py
+++ Test MVP (Selenium,Streamlit)/view.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import date
+from datetime import datetime
 
 class CourseView:
     def __init__(self):
@@ -32,7 +32,16 @@ class CourseView:
             course_title = st.text_input("Titolo del Corso", "Esempio: Analisi dei Dati")
             programme = st.text_area("Dettagli del Programma", "Campo opzionale: se necessario, inserire informazioni importanti sul corso")
             short_desc = st.text_input("Breve Descrizione", "Esempio: Analisi dei Dati Informatica")
-            start_date = st.date_input("Data di Pubblicazione", date(2023, 1, 1))
+            #start_date = st.date_input("Data di Pubblicazione", date(2023, 1, 1))
+
+            # Custom date input in Italian format
+            date_str = st.text_input("Data di Pubblicazione (GG/MM/AAAA)", "01/01/2023")
+
+            try:
+                start_date = datetime.strptime(date_str, "%d/%m/%Y").date()
+                #st.success(f"📅 Data selezionata: {start_date.strftime('%d/%m/%Y')}")
+            except ValueError:
+                st.error("Formato non valido. Usa GG/MM/AAAA.")
 
             # This is the button that will trigger the automation.
             submitted = st.form_submit_button("Crea Corso in Oracle")
