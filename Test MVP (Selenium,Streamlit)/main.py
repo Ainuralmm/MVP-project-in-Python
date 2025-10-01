02b971f1286bb634383370788988ef9b7ef6f9b0
Ainur <ainuralmm@gmail.com>
2025-10-01 18:32:23 +0200

    Removed all unnecessary hashtags

--- Test MVP (Selenium,Streamlit)/main.py
+++ Test MVP (Selenium,Streamlit)/main.py
@@ -28,11 +28,7 @@ if __name__ == "__main__":
             #start the application
             presenter.run(course_details)
 
-        # The presenter finished, which set automation_running = False and
-        # needs_rerun = True. We now trigger the final rerun immediately.
-        # if st.session_state.get("needs_rerun"):
-        #     st.session_state["needs_rerun"] = False
-        #     st.rerun()  # <--- THIS IS THE FINAL RERUN THAT REDRAWS THE BUTTON
+
     else:
         # safety: clear flag to avoid loop if something missing
         st.session_state["start_automation"] = False
